a50fca1b6666e7bc411806457fe60a7940cdc870
moyano9 <moyano9@hotmail.com>
2025-10-23 13:22:22 +0200

    migraciones y modelos

---
+++ database/migrations/2025_10_21_182432_create_productos_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('productos', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger("id_categoria");
+            $table->string("nombre");
+            $table->string("descripcion")->nullable();
+            $table->decimal("espacio");
+            $table->boolean("instalacion")->default(false);
+            $table->decimal("precio");
+            $table->integer("cantidad");
+            $table->string("imagen");
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign("id_categoria")->references("id")->on("categorias")->onDelete("cascade");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('productos');
+    }
+};
