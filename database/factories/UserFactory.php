2da6dc0112e76f09e8ea5de89a385f568d132b87
Raulhp579 <raulhenares5@gmail.com>
2025-10-27 17:48:21 +0100

    Guardando cambios en UserFactory antes de cambiar de rama

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Suscripcion;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
@@ -32,7 +33,7 @@ class UserFactory extends Factory
             'telefono' =>fake()->phoneNumber(),
             'direccion' => fake()->streetAddress(),
             'fecha_alta' => fake()->date(),
-            'id_subscripcion' =>Int::random(10) //no se si esto va
+            'id_subscripcion' =>Suscripcion::all()->random()->id 
         ];
     }
 
