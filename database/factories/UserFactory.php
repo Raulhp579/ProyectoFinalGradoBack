0afe601000dc97e1cf9402adb6f10372b28f9d46
javi <jjc0013@alu.medac.com>
2025-10-27 16:48:30 +0100

    factorias y modificaciones en user

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,9 +2,10 @@
 
 namespace Database\Factories;
 
-use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Support\Facades\Hash;
+use App\Models\Suscripcion;
 use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
@@ -32,7 +33,7 @@ class UserFactory extends Factory
             'telefono' =>fake()->phoneNumber(),
             'direccion' => fake()->streetAddress(),
             'fecha_alta' => fake()->date(),
-            'id_subscripcion' =>Int::random(10) //no se si esto va
+            'id_subscripcion' =>Suscripcion::all()->random()->id,
         ];
     }
 
