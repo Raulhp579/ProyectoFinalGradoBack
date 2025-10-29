8923f5369b34f8695ef143b0524e0f0ed32c841d
javi <jjc0013@alu.medac.com>
2025-10-29 18:50:36 +0100

    cambios en envioserviciocontroller

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Http\Controllers\EnvioServicioController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::post('/crearUsuario', [EnvioServicioController::class, "create"]) ->name("crearUsuario");
+Route::put('/crearUsuario', [EnvioServicioController::class, "update"])->name("actualizarUsuario");
+Route::put('/crearUsuario', [EnvioServicioController::class, "edit"])->name("editarUsuario");
+Route::delete('/crearUsuario', [EnvioServicioController::class, "destroy"])->name("borrarUsuario");
