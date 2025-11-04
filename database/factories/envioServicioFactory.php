4ef369b89f361d83d0ba505f3a571f9d271875e1
Raulhp579 <raulhenares5@gmail.com>
2025-11-04 18:20:00 +0100

    arreglo envioServicio

--- database/factories/envioServicioFactory.php
+++ database/factories/envioServicioFactory.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\envioServicio>
  */
-class envioServicioFactory extends Factory
+class EnvioServicioFactory extends Factory
 {
     /**
      * Define the model's default state.
