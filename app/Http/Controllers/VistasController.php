5e3f01c5d15db335b966168129fe471bdbde3b50
Raulhp579 <raulhenares5@gmail.com>
2025-11-19 20:42:49 +0100

    vista cartas a medio hacer

--- app/Http/Controllers/VistasController.php
+++ app/Http/Controllers/VistasController.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Tipo;
+
 class VistasController extends Controller
 {
     public function SuscripcionVista(){
-        return view('Suscripcion');
+        $tipos = Tipo::all();
+        return view('Suscripcion',['tipos'=>$tipos]);
     }
 }
---
+++ resources/views/components/Footer.blade.php
@@ -0,0 +1,3 @@
+<div>
+   <h1>foooter</h1>
+</div>
