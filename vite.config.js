833eab462905f1b22db6fea5d720b4df14caccad
Raulhp579 <raulhenares5@gmail.com>
2025-11-21 17:35:01 +0100

    arreglos vite header y footer

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js','resources/js/Suscripcion.js'],//añadir segun archivo
+            input: ['resources/css/app.css', 'resources/js/app.js','resources/js/Suscripcion.js','resources/js/header.js','resources/js/footer.js'],//añadir segun archivo
             refresh: true,
         }),
     ],
