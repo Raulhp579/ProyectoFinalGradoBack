60adde2bd582ca40f6b68cd0c60f7a495d351f2a
Raulhp579 <Raulhenares2002>
2025-11-23 14:30:37 +0100

    configuraciones para que vayan en produccion

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,19 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js','resources/js/Suscripcion.js','resources/js/header.js','resources/js/footer.js','resources/js/mail.js'],//añadir segun archivo
+            input: [
+                "resources/css/app.css",
+                "resources/js/app.js",
+                "resources/js/Suscripcion.js",
+                "resources/js/header.js",
+                "resources/js/footer.js",
+                "resources/js/mail.js",
+                "resources/css/inicio.css",
+                "resources/js/inicio.js",
+            ], //añadir segun archivo
             refresh: true,
         }),
     ],
